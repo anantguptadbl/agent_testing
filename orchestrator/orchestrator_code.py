a415614375e7d0a6be5973821a969d3faf969161
Ritesh Jena <riteshjena@gmail.com>
2026-01-25 23:40:32 +0530

    incremental_commit working api mock

--- orchestrator/orchestrator_code.py
+++ orchestrator/orchestrator_code.py
@@ -8,40 +8,49 @@ from fastapi.responses import JSONResponse
 from fastapi import Body
 import requests
 
+
 class AgentState(TypedDict):
     messages: list[dict]
-    #messages: Annotated[list[BaseMessage], "History"]
+    # messages: Annotated[list[BaseMessage], "History"]
+
 
 # Initialize connections to the 3 agents
 agent1 = RemoteRunnable("http://localhost:8001/agent1/process")
 agent2 = RemoteRunnable("http://localhost:8002/agent2/process")
 agent3 = RemoteRunnable("http://localhost:8003/agent3/process")
 
+
 def call_api1(state):
     url = "http://127.0.0.1:8004/api1/getdata1"
     params = {"input": "hello"}
     print("[DEBUG] Calling API1 with URL:", url, "and params:", params)
     print("Value of url and params:", url, params)
+    print("[DEBUG] Getting AP1 response json:", response.json())
     response = requests.get(url, params=params)
-    state['api1_result'] = response.json()
-    return response
+    state["messages"].append(response.json())
+    print("[DEBUG] State in AP1:", state)
+    return state
+
 
 async def call_a1(state):
-    if state['ap1i1_result']['output'] == 'hello':
+    if state["messages"][-1]["content"] == "hello":
         result = await agent1.ainvoke(state)
         print("[DEBUG] After call_a1, type:", type(result), "value:", result)
         return result
     return state
+
 
 async def call_a2(state):
     result = await agent2.ainvoke(state)
     print("[DEBUG] After call_a2, type:", type(result), "value:", result)
     return result
 
+
 async def call_a3(state):
     result = await agent3.ainvoke(state)
     print("[DEBUG] After call_a3, type:", type(result), "value:", result)
     return result
+
 
 # Build Orchestration Graph
 
@@ -57,11 +66,13 @@ builder.add_edge("a1", "a2")
 builder.add_edge("a2", "a3")
 builder.add_edge("a3", END)
 
+
 def build_orchestrator_graph(builder_object):
     """
     Takes a builder object and returns a compiled orchestrator graph instance.
     """
     return builder_object.compile()
+
 
 orchestrator_graph = build_orchestrator_graph(builder)
 
@@ -70,4 +81,5 @@ add_routes(app, orchestrator_graph, path="/orchestrate")
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
